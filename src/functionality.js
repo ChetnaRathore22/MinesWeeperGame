b397a55ee56b33b574b91fe9906845cb8c51a724
Chetna Rathore <114182392+ChetnaRathore22@users.noreply.github.com>
2023-08-04 12:53:43 +0530

    Update functionality.js

--- src/functionality.js
+++ src/functionality.js
@@ -1,6 +1,5 @@
 
-import audio from "./bomb.mp3"
-import React, { useState } from 'react';
+
 export const images=['https://images.unsplash.com/photo-1627213742863-46b836499634?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=789&q=80',
 'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zfGVufDB8fDB8fHww&w=1000&q=80','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-f7kQdE_MRGXMyfPP-QmFXiLKPGiwJduSfRXbMBK59IcjpJMdht1Myxfle4sPzsE-YZI&usqp=CAU',
 'https://thumbs.dreamstime.com/z/sunset-2486019.jpg?w=992','https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2','https://images.pexels.com/photos/1776268/pexels-photo-1776268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2','https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -12,26 +11,3 @@ export const images=['https://images.unsplash.com/photo-1627213742863-46b8364996
 
 
 
-const AudioPlayer = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  const audioRef = React.createRef();
-
-  const togglePlay = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-    }
-    setIsPlaying(!isPlaying);
-  };
-
-  return (
-    <div>
-      <audio ref={audioRef} src={audio } />
-      <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
-    </div>
-  );
-};
-
-export default AudioPlayer;
